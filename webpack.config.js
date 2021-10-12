50530d0ec5ad59fbb8c6504ca7205969a8cbffb7
Sho Kusano <rosylilly@aduca.org>
2021-10-12 15:29:08 +0900

    Fix font loading

--- webpack.config.js
+++ webpack.config.js
@@ -31,11 +31,7 @@ module.exports = {
       },
       {
         test: /\.(ttf|eot|woff|woff2|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
+        type: 'asset/resource',
       },
     ],
   },
